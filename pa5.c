46c9365432397cb9455a104cd6ca1fb9112c1b6f
Max Pintchouk <maxpintchouk4321@gmail.com>
2023-11-09 16:24:18 -0500

    timing multi

--- pa5.c
+++ pa5.c
@@ -1,33 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 #define MAX_VALUE 20
 #define DIM 3
-    
+#define MIN_DIM_POWER 3
+#define MAX_DIM_POWER 11    
+
 void init(const int dim, int * const m) {
     for (int i = 0; i < dim * dim; i++) {
         m[i] = rand() % (MAX_VALUE + 1);
     }
 }
 
-void multiply(const int dim, const int * const a, int * const b, int * const c) {
-    
-    /*
-     * MATRIX MULTIPLICATION
-     * cij = SUM k = 1->N of aik + bkj
-     *
-        7,2,13,9,   X  7,20,20,8,
-        8,6,0,0,    X  1,2,20,5,     
-        16,4,18,15, X  4,10,19,14,
-        1,6,14,4,   X  3,9,16,17,
-     *
-     * =
-     *  c[i,j] = a[i,0]b[0,j] = a[i,1]b[1,j] ...
-     *  c[0,0] = a[0,0]b[0,0] + a[0,1]b[1,0] + a[0,2]b[2,0] = a[0,3]b[3,0]
-     *  c[0,0] = (7)(7) + 2(1) + 13(4) + 9(3) = ...
-     *
-     */
-
+void multiply(const int dim, const int * const a, int * const b, int * const c) { 
     for (int i = 0; i < dim; i++) {
         for (int j = 0; j < dim; j++) {
             c[dim * i + j] = 0;
@@ -39,7 +25,6 @@ void multiply(const int dim, const int * const a, int * const b, int * const c) 
 }
 
 void transpose(const int dim, int * const m) {
-
     for (int i = 0; i < dim; i++) {
         for (int j = 0; j < i; j++) {
             int temp = m[i*dim + j];
@@ -50,22 +35,6 @@ void transpose(const int dim, int * const m) {
 }
 
 void multiply_transpose(const int dim, const int * const a, const int * const b_t, int * const c) {
-
-     /*
-     * MATRIX MULTIPLICATION BY ROWS 
-     * cij = SUM k = 1->N of aik + bjk
-     *
-        7,2,13,9,   X  7,20,20,8,
-        8,6,0,0,    X  1,2,20,5,     
-        16,4,18,15, X  4,10,19,14,
-        1,6,14,4,   X  3,9,16,17,
-     *
-     * =
-     *  c[i,j] = a[i,0]b[j,0] = a[i,1]b[j,1] ...
-     *  c[0,0] = a[0,0]b[0,0] + a[0,1]b[0,1] + a[0,2]b[0,2] = a[0,3]b[0,3]
-     *
-     */
-
     for (int i = 0; i < dim; i++) {
         for (int j = 0; j < dim; j++) {
             c[dim * i + j] = 0;
@@ -87,7 +56,23 @@ struct timeval run_and_time(
     const int * const a,
     int * const b,
     int * const c
-);
+) {
+    struct timeval start, end, elapsed;
+
+    gettimeofday(&start, NULL);
+    mult_func(dim, a, b, c);
+    gettimeofday(&end, NULL);
+
+    elapsed.tv_sec = end.tv_sec - start.tv_sec;
+    elapsed.tv_usec = end.tv_usec - start.tv_usec;
+
+    if (elapsed.tv_usec < 0) {
+        elapsed.tv_usec += 1000000;
+        elapsed.tv_sec -= 1;
+    }
+
+    return elapsed;
+}
 
 int verify(const int dim, const int * const c1, const int * const c2) {
     int TRUE = 1;
@@ -102,7 +87,6 @@ int verify(const int dim, const int * const c1, const int * const c2) {
 }
 
 void print(const int dim, const int * const m) {
-
     for (int i = 0; i < dim; i++) {
         for (int j = 0; j < dim; j++) {
             printf("%d,", m[dim * i + j]);
@@ -115,11 +99,33 @@ void run_test(const int dim) {
     int * a = (int*) calloc(dim * dim, sizeof(int));
     int * b = (int*) calloc(dim * dim, sizeof(int));
     int * c = (int*) calloc(dim * dim, sizeof(int));
+    int * c2 = (int*) calloc(dim * dim, sizeof(int));
     
     init(dim, a);
     init(dim, b);
+    
+    struct timeval time_multiply = run_and_time(multiply, dim, a, b, c);
+    struct timeval time_transpose_multiply = run_and_time(transpose_and_multiply, dim, a, b, c2);
 
-    printf("OPERAND MATRIX A\n"); 
+    int verif = verify(dim, c, c2);
+
+    printf("Standard multiplication: %ld seconds, %d microseconds\n",
+           (long)time_multiply.tv_sec, (int)time_multiply.tv_usec);
+    printf("Multiplication with transpose: %ld seconds, %d microseconds\n",
+           (long)time_transpose_multiply.tv_sec, (int)time_transpose_multiply.tv_usec);
+
+    if (verif == 1) {
+        printf("Results agree.\n");
+    } else {
+        printf("Results do not agree!\n");
+    }
+
+    free(a);
+    free(b);
+    free(c);
+    free(c2);
+
+    /*printf("OPERAND MATRIX A\n");
     print(dim, a);
     printf("OPERAND MATRIX B\n"); 
     print(dim, b);
@@ -142,9 +148,14 @@ void run_test(const int dim) {
     free(a);
     free(b);
     free(c);
+    free(temp_c);
+    */
 }
 
 int main() {
-    run_test(DIM);
-    return EXIT_SUCCESS;    
+    for (int power = MIN_DIM_POWER; power <= MAX_DIM_POWER; power++) {
+        int dim = 1 << power;
+        run_test(dim);
+    }
+    return EXIT_SUCCESS;
 }
